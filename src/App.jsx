6fc575972a92bd93e9d1aadda7745855e9729fd6
NikCodePro <vermanikhil7838@gmail.com>
2024-10-08 13:33:42 +0530

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-
 import Manager from './components/Manager'
 import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div >
-      <Navbar /><div className="absolute top-0 -z-10 h-full w-full bg-purple-50"><div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(139,90,196,0.5)] opacity-50 blur-[80px]">
-      </div></div>
+      <Navbar />
+      <div className="absolute top-0 -z-10 h-full w-full bg-purple-50">
+        <div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(139,90,196,0.5)] opacity-50 blur-[80px]">
+        </div>
+      </div>
       <Manager />
-
-      <h1 className="text-red-600">this is app.jsx</h1>
     </div>
   )
 }
